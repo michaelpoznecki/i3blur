152f61b8a771bfd72f60bf0c8a9923693920c7c5
andr01d <andr01d@webian>
2020-01-03 16:00:27 -0600

    Finds display resolution

--- i3blur.sh
+++ i3blur.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TMPBG=/tmp/screen.png
-RES=1920x1080
+RES=$(xdpyinfo | grep "dimensions:" | awk {"print $2"})
 
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -filter_complex "boxblur=8:1" -vframes 1 $TMPBG
 i3lock -i $TMPBG
