ac815553338e2b0fefbe882d4bcdd72162526ac9
rt-andr01d <37560904+rt-andr01d@users.noreply.github.com>
2020-01-12 18:33:22 -0600

    Update i3blur.sh

--- i3blur.sh
+++ i3blur.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 TMPBG=/tmp/screen.png
-RES=$(xdpyinfo | grep "dimensions:" | awk {"print $2"})
+RES=$(xdpyinfo | grep "dimensions:" | awk {'print $2'})
 
 ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -filter_complex "boxblur=8:1" -vframes 1 $TMPBG
 i3lock -i $TMPBG
